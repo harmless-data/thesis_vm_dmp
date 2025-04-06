7a5837b76c92936fba3e5162507cb778fa24c706
jacob thiessen <jacob.thiessen@fu-berlin.de>
2025-04-06 10:27:16 -0500

    woops

--- log_driver/toggle.sh
+++ log_driver/toggle.sh
@@ -4,7 +4,6 @@ DEV_NODE="/dev/mydevice"  # Replace with your device node if applicable
 
 if lsmod | grep -q "^$MODULE"; then
     echo "Module '$MODULE' is loaded. Unloading..."
-        pr_info("DMA buffer allocated: virt=%p, phys=0x%llx\n", kbuf, (unsigned long long)dma_handle);
     sudo rmmod $MODULE
     echo "Module '$MODULE' unloaded."
 else
